2f70c81afdc03d0618952e23a159a2c34bbe1e87
cerealkillerway <cerealkillerway@gmail.com>
2019-03-27 03:10:00 +0100

    renamed files

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cerealkiller:skelesite',
-    version: '4.0.5',
+    version: '5.0.0',
     summary: 'skeletor frontend helpers',
     // URL to the Git repository containing the source code for this package.
     git: '',
@@ -9,9 +9,14 @@ Package.describe({
 
 Package.onUse(function(api) {
     // namespace
-    api.addFiles(['namespace.js'], ['client', 'server']);
+    api.addFiles([
+        'namespace.js'
+    ],
+    ['client', 'server']);
+
 
     api.versionsFrom('METEOR@1.8.0.2');
+
 
     // dependencies
     api.use([
@@ -20,6 +25,7 @@ Package.onUse(function(api) {
         'fourseven:scss@4.5.4'
     ],
     ['client']);
+
 
     api.use([
         'mongo',
@@ -34,12 +40,13 @@ Package.onUse(function(api) {
     ],
     ['client', 'server']);
 
+
     // templates
     api.addFiles([
         'templates/menu/menu.html',
-        'templates/pageDefault.html',
-        'templates/pagePlaceholder.html',
-        'templates/langBar.html',
+        'templates/page-default.html',
+        'templates/page-placeholder.html',
+        'templates/lang-bar.html',
 
         'templates/panel/menus/menu-create.html',
         'templates/panel/menus/menus-list.html',
@@ -50,27 +57,30 @@ Package.onUse(function(api) {
     ],
     ['client']);
 
+
     // libraries
     api.addFiles([
         'lib/publish.js'
     ],
     ['server']);
 
+
     // event and helpers
     api.addFiles([
-        'events/menuEvents.js',
-        'events/pageDefaultEvents.js',
-        'events/pagePlaceholderEvents.js',
-        'events/langBar.js',
+        'events/menu-events.js',
+        'events/page-default-events.js',
+        'events/page-placeholder-events.js',
+        'events/lang-bar.js',
         'events/panel-events.js',
 
-        'helpers/pageDefaultHelpers.js',
-        'helpers/menuHelpers.js',
-        'helpers/pagePlaceholderHelpers.js',
-        'helpers/langBar.js',
+        'helpers/page-default-helpers.js',
+        'helpers/menu-helpers.js',
+        'helpers/page-placeholder-helpers.js',
+        'helpers/lang-bar.js',
         'helpers/panel-helpers.js'
     ],
     ['client']);
+
 
     api.addFiles([
         'lib/collections/menus.js',
@@ -80,6 +90,7 @@ Package.onUse(function(api) {
         'lib/auth-match.js'
     ],
     ['client', 'server']);
+
 
     // styles
     api.addFiles([
@@ -91,6 +102,7 @@ Package.onUse(function(api) {
     // exports
     api.export(['Skelesite']);
 });
+
 
 Package.onTest(function(api) {
     api.use([
